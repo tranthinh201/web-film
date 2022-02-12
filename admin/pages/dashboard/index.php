ffdfe473e70f833485d7bd5f934d01a3b98c1841
thinhtran201 <console.namee@gmail.com>
2022-02-12 17:17:38 +0700

    update dashboard

--- admin/pages/dashboard/index.php
+++ admin/pages/dashboard/index.php
@@ -553,7 +553,7 @@
       <div class="card-header pb-0 pt-3">
         <div class="float-start">
           <h5 class="mt-3 mb-0">Material UI Configurator</h5>
-          <p>See our dashboard options.</p>
+          <p></p>
         </div>
         <div class="float-end mt-4">
           <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
