943cccdb1893e509c7a6c74d123df0bb1198585d
tranthinh201 <console.namee@gmail.com>
2022-06-16 18:38:39 +0700

    login admin

--- admin/pages/sign-in.php
+++ admin/pages/sign-in.php
@@ -1,185 +1,51 @@
-<?php
-session_start();
-if (isset($_SESSION['user'])) {
-  header('location:./index.php');
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-  <title>
-    Đăng nhập
-  </title>
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-  <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <!-- Material Icons -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-  <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
+	<title>Đăng nhập</title>
+	<meta charset="UTF-8">
+
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+<!--===============================================================================================-->
 </head>
-
-<body class="bg-gray-200">
-  <div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-      <div class="col-12">
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg blur border-radius-xl top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
-          <div class="container-fluid ps-2 pe-0">
-            <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon mt-2">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </span>
-            </button>
-            <div class="collapse navbar-collapse" id="navigation">
-              <ul class="navbar-nav mx-auto">
-                <!--                 <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="../pages/dashboard.html">
-                    <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
-                    Dashboard
-                  </a>
-                </li> -->
-
-                <li class="nav-item">
-                  <!--                   <a class="nav-link me-2" href="sign-up.php">
-                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
-                    Đăng ký
-                  </a> -->
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="sign-in.php">
-                    <i class="fas fa-key opacity-6 text-dark me-1"></i>
-                    Đăng nhập
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <!-- End Navbar -->
-      </div>
-    </div>
-  </div>
-  <main class="main-content  mt-0">
-    <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
-      <span class="mask bg-gradient-dark opacity-6"></span>
-      <div class="container my-auto">
-        <div class="row">
-          <div class="col-lg-4 col-md-8 col-12 mx-auto">
-            <div class="card z-index-0 fadeIn3 fadeInBottom">
-              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Đăng nhập</h4>
-                  <div class="row mt-3">
-                    <div class="col-2 text-center ms-auto">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-facebook text-white text-lg"></i>
-                      </a>
-                    </div>
-                    <div class="col-2 text-center px-1">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-github text-white text-lg"></i>
-                      </a>
-                    </div>
-                    <div class="col-2 text-center me-auto">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-google text-white text-lg"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body">
-                <form role="form" action="./si_submit.php" method="POST" class="text-start">
-                  <p style="margin-bottom:25px;color:red;">
-                    <?php
-                    if (isset($_SESSION['thongbao'])) {
-                      echo $_SESSION['thongbao'];
-                      session_unset();
-                    }
-                    ?>
-                  </p>
-                  <p style="margin-bottom:25px;color:green;">
-                    <?php
-                    if (isset($_SESSION['thongbaoo'])) {
-                      echo $_SESSION['thongbaoo'];
-                      session_unset();
-                    }
-                    ?>
-                  </p>
-                  <div class="input-group input-group-outline my-3">
-                    <label class="form-label">Tên tài khoản</label>
-                    <input type="text" name="username" class="form-control">
-                  </div>
-                  <div class="input-group input-group-outline mb-3">
-                    <label class="form-label">Mật khẩu</label>
-                    <input type="password" name="password" class="form-control">
-                  </div>
-                  <div class="form-check form-switch d-flex align-items-center mb-3">
-                    <input class="form-check-input" type="checkbox" id="rememberMe">
-                    <label class="form-check-label mb-0 ms-2" for="rememberMe">Lưu mật khẩu</label>
-                  </div>
-                  <div class="text-center">
-
-                    <input type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2" name="submit" value="Đăng nhập">
-                  </div>
-                  <!--                   <p class="mt-4 text-sm text-center">
-                    Chưa có tài khoản ?
-                    <a href="sign-up.php" class="text-primary text-gradient font-weight-bold">Đăng ký</a>
-                  </p> -->
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <footer class="footer position-absolute bottom-2 py-2 w-100">
-        <div class="container">
-          <div class="row align-items-center justify-content-lg-between">
-            <div class="col-12 col-md-6 my-auto">
-              <div class="copyright text-center text-sm text-white text-lg-start">
-                © <script>
-                  document.write(new Date().getFullYear())
-                </script>,
-                được tạo <i class="fa fa-heart" aria-hidden="true"></i> bởi
-                <a href="#" class="font-weight-bold text-white" target="_blank">HTcinema</a>
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
-  </main>
-  <!--   Core JS Files   -->
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
+<body>
+<p style="margin-bottom:25px;color:red;">
+      <?php
+      if (isset($_SESSION['thongbao'])) {
+        echo $_SESSION['thongbao'];
+        session_unset();
       }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/material-dashboard.min.js?v=3.0.0"></script>
+      ?>
+    </p>
+    <p style="margin-bottom:25px;color:green;">
+      <?php
+      if (isset($_SESSION['thongbaoo'])) {
+        echo $_SESSION['thongbaoo'];
+        session_unset();
+      }
+      ?>
+    </p>
+<form class="form-structor" action="./si_submit.php" method="POST">
+	<div class="signup">
+		<h2 class="form-title" id="signup">Đăng nhập</h2>
+		<div class="form-holder">
+			<input type="text" class="input" placeholder="Username" name="username"/>
+			<input type="password" class="input" placeholder="Password" name="password" />
+		</div>
+		<button class="submit-btn" type="submit" name="submit">Đăng nhập</button>
+	</div>
+	<div class="login slide-up">
+		<div class="center">
+			<!-- <h2 class="form-title" id="login"><span>or</span>Log in</h2> -->
+			<div class="form-holder">
+				<input type="email" class="input" placeholder="Email" />
+				<input type="password" class="input" placeholder="Password" />
+			</div>
+			
+		</div>
+	</div>
+</form>
 </body>
-
 </html>
+
+
