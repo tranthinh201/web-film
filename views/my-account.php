3bc5199180cf686316672675ccc290bc59b31ee0
tranthinh201 <console.namee@gmail.com>
2022-06-17 21:10:50 +0700

    final

--- views/my-account.php
+++ views/my-account.php
@@ -4,6 +4,7 @@
     <?php
         session_start();
         include('../config/db.php');
+        include('../config/sql_cn.php');
         include('./include/library.php');
         $ACCOUNT = 'SELECT id FROM khach_hang where email = "' . $_SESSION['user-client'] . '"';
         $query_up = mysqli_query($connect, $ACCOUNT);
@@ -18,37 +19,75 @@
 
     <div class="main">
         <div class="container">
-            <h2 class="" style="text-align: center; font-weight:bold;">
-                Danh sách vé đã đặt
-            </h2>
             <div class="infor-ticket">
                 <?php
-                    $SQL = "SELECT ve_ban.id ve_id, ve_ban.ngay_ban, ve_ban.tong_tien, ve_ban.suat_chieu_id, suat_chieu.gio_bat_dau, suat_chieu.gio_ket_thuc, phim.ten, phim.hinh_anh, suat_chieu.phong_chieu_id
-                            FROM ve_ban, khach_hang, suat_chieu, phim
-                            WHERE khach_hang.id = ve_ban.khach_hang_id 
-                            AND suat_chieu.id = ve_ban.suat_chieu_id
-                            AND suat_chieu.phim_id = phim.id 
-                            AND khach_hang.id = ve_ban.khach_hang_id and khach_hang.id = '".$row_up['id']."'";
-                    $result = executeResult($SQL);
-                    foreach($result as $row) {
-                        echo'
-                            <ul>
-                                <li>'.$row['ve_id'].'</li>
-                                <li>'.$row['tong_tien'].'</li>
-                                <li>'.$row['gio_bat_dau'].'</li>
-                                <li>'.$row['gio_ket_thuc'].'</li>
-                                <li>'.$row['ten'].'</li>
-                                <li>
-                                    <img src="../image/phim/'.$row['hinh_anh'].'" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="'.$row['ten'].'">
-                                </li>
-                                <li>'.$row['phong_chieu_id'].'</li>
-                            </ul>
+
+                    $SQLSC = "SELECT DISTINCT ve_ban.suat_chieu_id, phim.ten, TIME(suat_chieu.gio_bat_dau), TIME(suat_chieu.gio_ket_thuc), phim.hinh_anh, ve_ban.ngay_ban, suat_chieu.ngay_chieu
+                                FROM ve_ban, khach_hang, suat_chieu, phim
+                                WHERE khach_hang.id = ve_ban.khach_hang_id 
+                                AND suat_chieu.id = ve_ban.suat_chieu_id 
+                                AND khach_hang.id = ve_ban.khach_hang_id
+                                AND suat_chieu.phim_id = phim.id
+                                AND khach_hang.id = '".$row_up['id']."'
+                                AND ve_ban.da_huy = 0";
+                    $SC = executeResult($SQLSC);
+
+                    if(empty($result)) {
+                        echo '<div class ="empty-result">
+                                Bạn chưa mua vé nào của chúng tôi ~.~
+                                <br>
+                            <a href="./movies-list.php">Đặt vé nào <3</a>
+                            </div>';
+                    } else {
+                        echo '
+                        <h2 class="" style="text-align: center; font-weight:bold;">
+                            Danh sách vé đã đặt
+                        </h2>
                         ';
+                        foreach($SC as $row) {
+                            echo'
+                            <div class="box-ticket">
+                                <div class="image">
+                                    <img src="../image/phim/'.$row['hinh_anh'].'"/>
+                                </div>
+                                <div class="box-infor">
+                                    <span>Phim: '.$row['ten'].'</span>
+                                    <div class="">
+                                        <span>Thời gian chiếu: </span><span>'.$row['TIME(suat_chieu.gio_bat_dau)'].' ~ '.$row['TIME(suat_chieu.gio_ket_thuc)'].'</span>
+                                    </div>
+                                    <span>Ngày: '.$row['ngay_chieu'].'</span>
+                                </div>
+                            </div>
+                            ';
+                            $VE = "SELECT ve_ban.id, ghe_ngoi.vi_tri_day, ghe_ngoi.vi_tri_cot, ve_ban.tong_tien 
+                                FROM suat_chieu, khach_hang, ve_ban, ghe_ngoi
+                                WHERE ve_ban.suat_chieu_id = suat_chieu.id
+                                AND ve_ban.ghe_id = ghe_ngoi.id
+                                AND ve_ban.suat_chieu_id = '".$row['suat_chieu_id']."'
+                                AND khach_hang.id = ve_ban.khach_hang_id
+                                AND khach_hang.id = '".$row_up['id']."'
+                                AND ve_ban.da_huy = 0";
+                               
+                            $RS = mysqli_query($connect, $VE);
+                            $sum = 0;
+                            echo'<div class="box-remove">';
+                            echo'<div style="font-size:18px;">Danh sách ghế đã đặt:</div>';
+                            while($rows = mysqli_fetch_array($RS)) {?>
+                                    <div class="box-ticket-remove">
+                                        <?=$rows['vi_tri_cot']?><?=$rows['vi_tri_day']?>
+                                        <a href="./include/delete-ticket.php?id=<?=$rows['id']?>" onclick="return Del('<?= $rows['id'] ?>')"><i class="fas fa-times"></i></a>
+                                    </div>
+                                    <?php $sum += $rows['tong_tien']?>
+                            <?php } 
+                            echo '</div>';
+                            echo '<span style="font-size:18px;">Tổng tiền: '.$sum.'</span>';
+                        }
+
                     }
-                ?>
+                    ?>
+            </div>
             </div>
         </div>
-    </div>
     <?php
         include('./include/footer.php');
     ?>
@@ -62,5 +101,97 @@
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
 
+    .read-more-state {
+            display: none;
+    }
+
+    .read-more-target {
+        opacity: 0;
+        max-height: 0;
+        font-size: 0;
+        transition: .25s ease;
+    }
+
+    .read-more-state:checked ~ .read-more-wrap .read-more-target {
+        opacity: 1;
+        font-size: inherit;
+        max-height: 999em;
+    }
+
+    .read-more-state ~ .read-more-trigger:before {
+        content: 'Show more';
+    }
+
+    .read-more-state:checked ~ .read-more-trigger:before {
+        content: 'Show less';
+    }
+
+    .read-more-trigger {
+        cursor: pointer;
+        display: inline-block;
+        padding: 0 .5em;
+        color: #666;
+        font-size: .9em;
+        line-height: 2;
+        border: 1px solid #ddd;
+        border-radius: .25em;
+    }
+
+    /* Other style */ 
+    .empty-result {
+        text-align: center;
+        padding: 300px;
+        font-size: 40px;
+        font-weight: bold;
+    }
+    
+    .box-ticket {
+        display: flex;
+    }
+
+    .box-ticket > .image > img {
+        width: 150px;
+    }
+
+    .box-ticket > .box-infor {
+        margin-left: 20px;
+    }
+
+    .box-ticket > .box-infor {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .box-ticket > .box-infor span{
+        font-size: 16px;
+        font-weight: 700;
+    }
+
+    .box-infor > button {
+        border: 1px solid;
+        color: orange;
+        background-color: #ddd;
+        width: 100px;
+    }
+
+    .box-ticket-remove {
+        border: 1px solid;
+        width: 50px;
+        display: flex;
+        justify-content: space-around;
+        margin: 5px;
+    }
+   
+    .box-remove {
+        display: flex;
+        align-items: center;
+    }
+        
 
 </style>
+
+<script type="text/javascript">
+    function Del(id) {
+        return confirm("Bạn có chắc chắn muốn huỷ vé này không?");
+    }
+</script>
