7927243b79031a8d597a67275bab47d9fb46a354
tranthinh201 <console.namee@gmail.com>
2022-06-18 12:59:42 +0700

    final

--- views/final.php
+++ views/final.php
@@ -1,40 +1,38 @@
 <?php
        include('../config/db.php');
+       require_once("../config/sql_cn.php");
        session_start();
 
            // code...
-           $length = count($_SESSION['counter']);
-           $id = $_GET['id'];
-           $date = date('Y-m-d');
-           $ACCOUNT = 'SELECT id FROM khach_hang where email = "' . $_SESSION['user-client'] . '"';
+            $length = count($_SESSION['counter']);
+            $id = $_GET['id'];
+            $date = date('Y-m-d');
+            $ACCOUNT = 'SELECT id FROM khach_hang where email = "' . $_SESSION['user-client'] . '"';
             $query_up = mysqli_query($connect, $ACCOUNT);
             $row_up = mysqli_fetch_assoc($query_up);
 
-
             for($i = 0; $i < $length; $i++) 
             {
-                $GHE = "SELECT * FROM ghe_ngoi, loai_ghe 
+                $GHE = "SELECT ghe_ngoi.vi_tri_day, ghe_ngoi.vi_tri_cot, ghe_ngoi.id, loai_ghe.phu_thu FROM ghe_ngoi, loai_ghe 
                         WHERE ghe_ngoi.loai_ghe_id = loai_ghe.id 
                         AND ghe_ngoi.id = ".$_SESSION['counter'][$i]."";
-              
                 $result = executeResult($GHE);
                 foreach ($result as $row) {
                     $SQL = "INSERT INTO ve_ban(ghe_id, suat_chieu_id, ngay_ban, tong_tien, khach_hang_id, da_huy) VALUES(".$_SESSION['counter'][$i].", '".$id."', '".$date."', ".$row['phu_thu'].", '".$row_up['id']."', 0)";
-                    var_dump($SQL);
-                    $query = mysqli_query($connect, $SQL);
+                    $SQLC = "SELECT ve_ban.suat_chieu_id, ve_ban.ghe_id 
+                            FROM ve_ban 
+                            WHERE ve_ban.suat_chieu_id = '$id' AND ve_ban.ghe_id = ".$_SESSION['counter'][$i]."";
 
-                }
-                      
+                
+                    $old = mysqli_query($conn, $SQLC);
+                    if (mysqli_num_rows($old) > 0) {
+                        echo '<script type="text/javascript">alert("Ghế '.$row['vi_tri_day'].''.$row['vi_tri_cot'].' đã bị đặt. Bạn chậm hơn mất r :))")</script>'; 
+                    } else {
+                        $query = mysqli_query($connect, $SQL);
+                    }
 
-                    if ($query==true) {
-                        // code...
-                         header("Location:./index.php");
-                    }
-                    else
-                    {
-                        header("Location:./lich-chieu.php");
-                    }
+                } 
             }
-
+            var_dump($query);
 
 ?>
